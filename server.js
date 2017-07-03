5cd02a606b25e4ccce7bddcb06a4cdc2ac7985b1
sudharsan.tk@gmail.com <sudharsan_tk@yahoo.co.in>
2017-07-03 11:46:56 +0800

    fixes

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-
- 
  
 
 
 const http = require('http')  
- 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 const requestHandler = (request, response) => {  
   console.log(request.url)
@@ -16,10 +12,10 @@ const requestHandler = (request, response) => {
 
 const server = http.createServer(requestHandler)
 
-server.listen(port,ip, (err) => {  
+server.listen(port, (err) => {  
   if (err) {
     return console.log('something bad happened', err)
   }
 
-  console.log(`server is listening on ${server_port}`)
+  console.log(`server is listening on ${port}`)
 })
